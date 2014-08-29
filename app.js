0019b65a302ca74a2bdae1690e7a873721f9ff0b
Fetra <fetra@tag-ip.com>
2014-08-29 17:01:55 +0300

    Add duration

--- app.js
+++ app.js
@@ -13,33 +13,37 @@ angular.module('demo', ['notifications']).
 
     $scope.makeInfo = function(){
       if($scope.notiTitle !== '' || $scope.notiText !== ''){
-        $notification.info($scope.notiTitle, $scope.notiText);
+        $notification.info($scope.notiTitle, $scope.notiText, null, $scope.notiDuration);
         $scope.notiText = '';
         $scope.notiTitle = '';
+        $scope.notiDuration = 5000;
       }
     };
 
     $scope.makeWarning = function(){
       if($scope.notiTitle !== '' || $scope.notiText !== ''){
-        $notification.warning($scope.notiTitle, $scope.notiText);
+        $notification.warning($scope.notiTitle, $scope.notiText, null, $scope.notiDuration);
         $scope.notiText = '';
         $scope.notiTitle = '';
+        $scope.notiDuration = 5000;
       }
     };
 
     $scope.makeError = function(){
       if($scope.notiTitle !== '' || $scope.notiText !== ''){
-        $notification.error($scope.notiTitle, $scope.notiText);
+        $notification.error($scope.notiTitle, $scope.notiText, null, $scope.notiDuration);
         $scope.notiText = '';
         $scope.notiTitle = '';
+        $scope.notiDuration = 5000;
       }
     };
 
     $scope.makeSuccess = function(){
       if($scope.notiTitle !== '' || $scope.notiText !== ''){
-        $notification.success($scope.notiTitle, $scope.notiText);
+        $notification.success($scope.notiTitle, $scope.notiText, null, $scope.notiDuration);
         $scope.notiText = '';
         $scope.notiTitle = '';
+        $scope.notiDuration = 5000;
       }
     };
   });
